6947fdab73c26f00146f54811d8e6f0e71c5a7c7
Damian Martinez <damianagustinmaartinez@gmail.com>
2021-09-14 17:20:43 -0300

    listo tabladiagonal y cruz

--- tabladiagonal.php
+++ tabladiagonal.php
@@ -1,13 +1,13 @@
 <table border="1">
 <?php 
-	$cant_filas = $_GET['cfilas'];
-	$cant_col = $_GET['ccolumnas'];
+	$cant_filas = 13;
+	$cant_col = 13;
 	?>
 	<?php for ($filas=1;$filas <=$cant_filas;$filas++){ ?>
 		<tr>
 		<?php for ($col=1;$col<=$cant_col;$col++) { ?>
 		<?php 
-		if ($col==$filas){
+		if ($col==$filas or ($col + $filas == $cant_filas + 1)){
 			$color = "red";
 		} else {
 			$color = "white";
