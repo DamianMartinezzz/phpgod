4f6e519509184d09ae2b2927ba1e16a2b8db47aa
Damian Martinez <damianagustinmaartinez@gmail.com>
2021-09-14 16:29:20 -0300

    modifique el formulario

--- get.php
+++ get.php
@@ -1,11 +1,12 @@
 <form action="tablacolor.php" method="GET">
-filas: <input type="text" name="cfilas"> <br>
-columnas: <input type="text" name="ccolumnas"> <br>
-color: <input type="text" name="ccolor"> <br>
-<input type="submit">
+A continuacion mediante datos creara una tabla y formara una cruz del color que usted quiera, debe escribir el color en ingles.
+<br><br>
+Filas: <input type="text" name="cfilas"> <br>
+Columnas: <input type="text" name="ccolumnas"> <br>
+Color: <input type="text" name="ccolor"> <br>
+<input type="submit" value="Crear tabla">
 </form>
 
 <pre>
 <?php
-
 ?>
