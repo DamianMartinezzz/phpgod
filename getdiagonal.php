2d9896370c62159d4c2a1a467268f114fb7e9d14
Damian Martinez <damianagustinmaartinez@gmail.com>
2021-09-14 17:32:58 -0300

    tarea hecha tablas

--- getdiagonal.php
+++ getdiagonal.php
@@ -1,9 +1,9 @@
-<form action="tablacolor.php" method="GET">
+<form action="tabladiagonal.php" method="GET">
 A continuacion mediante datos creara una tabla y formara una cruz del color que usted quiera, debe escribir el color en ingles.
 <br><br>
-Filas: <input type="text" name="cfilas"> <br>
-Columnas: <input type="text" name="ccolumnas"> <br>
-Color: <input type="text" name="ccolor"> <br>
+Filas: <input type="text" name="dfilas"> <br>
+Columnas: <input type="text" name="dcolumnas"> <br>
+Color: <input type="text" name="dcolor"> <br>
 <input type="submit" value="Crear tabla">
 </form>
 
--- tabladiagonal.php
+++ tabladiagonal.php
@@ -1,14 +1,14 @@
 <table border="1">
 <?php 
-	$cant_filas = 13;
-	$cant_col = 13;
+	$cant_filas = $_GET['dfilas'];
+	$cant_col = $_GET['dfilas'];
 	?>
 	<?php for ($filas=1;$filas <=$cant_filas;$filas++){ ?>
 		<tr>
 		<?php for ($col=1;$col<=$cant_col;$col++) { ?>
 		<?php 
-		if ($col==$filas or ($col + $filas == $cant_filas + 1)){
-			$color = "red";
+		if ($col==$filas or ($col + $filas == $cant_filas + 1) &&($col + $filas == $cant_col + 1)){
+			$color = $_GET['dcolor'];
 		} else {
 			$color = "white";
 		}
